a622de453c14baabf91b593debd6a8c19b67bd8b
Grzegorz Kusmierz <g.kusmierz@gmail.com>
2022-05-20 19:16:09 +0200

    Initial ES5 bundle.js

---
+++ webpack.config.ts
@@ -0,0 +1,29 @@
+import path from "path";
+import { Configuration } from "webpack";
+
+const config: Configuration = {
+  entry: "./dist/esm/sdk.js",
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)?$/,
+        //exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-typescript"],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "../bundle.js",
+  }
+};
+
+export default config;
